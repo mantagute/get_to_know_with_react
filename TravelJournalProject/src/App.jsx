e0266f17d30397dad286372832e753643df396ea
Mantagute <joaogabrieliuzviakmantagute@gmail.com>
2026-02-02 21:04:42 -0300

    applied props concept using map to get data from externalservices and finished the TravelJournal

--- TravelJournalProject/src/App.jsx
+++ TravelJournalProject/src/App.jsx
@@ -1,16 +1,25 @@
 import Header from './components/Header';
 import Entry from './components/Entry';
+import data from './data.js';
 import './App.css'
 
 function App() {
+
+  const EntryElements= data.map((travel) => {
+      return (
+        <Entry
+          key={travel.id}
+          object={travel}
+        />
+      )
+  })
 
   return (
     <>
       <Header/>
       <main className='container'>
-        <Entry/>
+        {EntryElements}
       </main>
-
     </>
   )
 }
