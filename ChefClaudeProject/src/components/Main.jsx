82efb443ff89d0a4b723057fd793f4dc1d52e73a
Mantagute <joaogabrieliuzviakmantagute@gmail.com>
2026-02-03 21:34:36 -0300

    refactor Form Submission

--- ChefClaudeProject/src/components/Main.jsx
+++ ChefClaudeProject/src/components/Main.jsx
@@ -1,16 +1,44 @@
+import React from 'react';
 import './Main.css'
 
+
 export default function Main() {
+
+    const [ingredients, setIngredients] = React.useState([]);
+
+    const ingredientsListItems = ingredients.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+    ))
+
+
+    function addNewIngredient(formData) {
+        const newIngredient = formData.get('ingredient');
+        setIngredients(prevState => 
+            [...prevState,
+                newIngredient
+            ]);     
+    }
+
     return (
         <main>
-            <form className='add-ingredient-form'>
+
+            <form className='add-ingredient-form' action={addNewIngredient}>
+                
                 <input 
                     type="text" 
                     aria-label='Add ingredient' 
                     placeholder='e.g oregano'
+                    name='ingredient'
                 />
+
                 <button>Add ingredient</button>
+
             </form>
+
+            <ul>
+                {ingredientsListItems}
+            </ul>
+
         </main>
     )
 }
