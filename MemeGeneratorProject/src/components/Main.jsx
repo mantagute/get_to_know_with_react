5fa3b229458b67c2e9cfa58ed59b74cf260a391b
Mantagute <joaogabrieliuzviakmantagute@gmail.com>
2026-02-05 18:00:10 -0300

    getting external images from a API using useEffect and showing it randomly in the application

--- MemeGeneratorProject/src/components/Main.jsx
+++ MemeGeneratorProject/src/components/Main.jsx
@@ -1,6 +1,42 @@
+import {useState} from 'react';
+import { useEffect } from 'react';
 import './Main.css'
 
 export default function Main() {
+
+    const [memeInfo, setMemeInfo] = useState({
+        imageUrl: "http://i.imgflip.com/1bij.jpg",
+        topText: 'One does not simply',
+        bottomText: 'Walk into Mordor'
+    })
+
+    function handleChange(event) {
+        const {value, name} = event.currentTarget;
+        setMemeInfo(prevState => (
+            {...prevState,
+            [name]: value
+            }
+        ))
+    }
+
+    const [allMemes, setAllMemes] = useState([])
+
+    useEffect(() => {
+            fetch('https://api.imgflip.com/get_memes')
+            .then(res => res.json())
+            .then(data => setAllMemes(data.data.memes))
+    }, [])
+
+    function getRandomMemeImage() {
+        const randomIndex = Math.floor(Math.random() * allMemes.length);
+        setMemeInfo(prevState => (
+            {
+                ...prevState,
+                imageUrl: allMemes[randomIndex].url
+            }
+        ))
+    }
+
     return (
 
         <main>
@@ -11,6 +47,8 @@ export default function Main() {
                         type="text"
                         placeholder="One does not simply"
                         name="topText"
+                        onChange={handleChange}
+                        value={memeInfo.topText}
                     />
                 </label>
 
@@ -19,15 +57,17 @@ export default function Main() {
                         type="text"
                         placeholder="Walk into Mordor"
                         name="bottomText"
+                        onChange={handleChange}
+                        value={memeInfo.bottomText}
                     />
                 </label>
-                <button>Get a new meme image 🖼</button>
+                <button onClick={getRandomMemeImage}>Get a new meme image 🖼</button>
             </div>
 
             <div className="meme">
-                <img src="http://i.imgflip.com/1bij.jpg" />
-                <span className="top">One does not simply</span>
-                <span className="bottom">Walk into Mordor</span>
+                <img src={memeInfo.imageUrl} />
+                <span className="top">{memeInfo.topText}</span>
+                <span className="bottom">{memeInfo.bottomText}</span>
             </div>
 
         </main>
